84c54eefb13939ab0f41b4d5cb184c0cfbfd42f1
Austin <aokincaid@gmail.com>
2015-09-25 09:48:47 -0700

    Add emberfire and firebase database to app

--- app/models/answer.js
+++ app/models/answer.js
@@ -3,5 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr(),
   body: DS.attr(),
-  question: belongsTo('question', {async: true})
+  question: DS.belongsTo('question', {async: true})
 });
--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'quest-answer',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://questAnswer.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
