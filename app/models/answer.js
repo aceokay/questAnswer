790484889b16e2de8ede93e008a70e50d23a71e3
Austin <aokincaid@gmail.com>
2015-09-25 09:40:41 -0700

    Add answer model with basic attributes and relationship to question model

--- app/models/answer.js
+++ app/models/answer.js
@@ -2,7 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr(),
-  title: DS.attr(),
   body: DS.attr(),
-  answers: DS.hasMany('answer', {async: true})
+  question: belongsTo('question', {async: true})
 });
